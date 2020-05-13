6951db71160582727f9a54d44bdfb83246b4227f
DionatanPO <dionatanoliveirapc@gmail.com>
2020-05-13 15:29:25 -0300

    Atualizando 1.0.23 pc desk

---
+++ src/main/java/com/example/Api_hotel/model/Reserva.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Api_hotel.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Dionatan
+ */
+public class Reserva {
+    private Long id;
+    private Date  data_entrada;
+    private Date  data_saida;
+    private Hospede hospede;
+    private Apartamento apartamento;
+
+    public Reserva() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getData_entrada() {
+        return data_entrada;
+    }
+
+    public void setData_entrada(Date data_entrada) {
+        this.data_entrada = data_entrada;
+    }
+
+    public Date getData_saida() {
+        return data_saida;
+    }
+
+    public void setData_saida(Date data_saida) {
+        this.data_saida = data_saida;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public Apartamento getApartamento() {
+        return apartamento;
+    }
+
+    public void setApartamento(Apartamento apartamento) {
+        this.apartamento = apartamento;
+    }
+    
+    
+}
